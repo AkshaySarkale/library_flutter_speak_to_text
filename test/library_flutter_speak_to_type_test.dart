619c63f307ebf58cdd268082a8e3fb43c374c26b
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-02 12:03:00 +0530

    add export line

--- test/library_flutter_speak_to_type_test.dart
+++ test/library_flutter_speak_to_type_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:library_flutter_speak_to_type/library_flutter_speak_to_type.dart';
+export 'package:library_flutter_speak_to_type/speakToText.dart';
 
 void main() {
   test('adds one to input values', () {
