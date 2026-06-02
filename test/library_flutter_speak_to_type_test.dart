5109e0cbacfeb772fe11466f4271b4e042fead51
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-02 11:53:46 +0530

    remove updates

--- test/library_flutter_speak_to_type_test.dart
+++ test/library_flutter_speak_to_type_test.dart
@@ -3,4 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:library_flutter_speak_to_type/library_flutter_speak_to_type.dart';
 
 void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
 }
